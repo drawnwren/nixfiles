e61e02612d8cfbfdc0a36754616d4fd142d1c22b
drawnwren <drawnwren@gmail.com>
2024-11-10 10:21:01 -0800

    x

--- configuration.nix
+++ configuration.nix
@@ -32,9 +32,13 @@ in
     experimental-features = [ "nix-command" "flakes" ];
     substituters = [
       "https://cuda-maintainers.cachix.org"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
   services.supergfxd.enable = true;
