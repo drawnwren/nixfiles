104d0117b57a5e3df2722557f444dfe81cd81efa
wren <drawnwren@gmail.com>
2026-01-23 10:19:36 -0800

    Update wing.lua

--- config/nvim/lua/wing.lua
+++ config/nvim/lua/wing.lua
@@ -133,6 +133,11 @@ require('nvim-treesitter').setup {
   },
 }
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
+
 vim.filetype.add({
   extension = {
     msg = "cpp",
