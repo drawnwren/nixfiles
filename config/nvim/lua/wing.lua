3788cf4f8eeda3db3061444ac9507f68180effa7
wren <drawnwren@gmail.com>
2026-01-23 10:28:15 -0800

    Update wing.lua

--- config/nvim/lua/wing.lua
+++ config/nvim/lua/wing.lua
@@ -113,8 +113,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { '<filetype>' },
   callback = function() vim.treesitter.start() end,
 })
-vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.wo[0][0].foldmethod = 'expr'
+
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
 vim.filetype.add({
