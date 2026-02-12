5bb8271ac9d101866c25c3143ca443592a62ff06
wren <drawnwren@gmail.com>
2026-02-12 13:24:20 -0800

    Update nix.lua

--- config/nvim/lua/languages/nix.lua
+++ config/nvim/lua/languages/nix.lua
@@ -1,11 +1,12 @@
 local utils = require("lsp_utils")
+local nixpkgs_input = os.getenv("NIXD_NIXPKGS_INPUT") or "nixpkgs"
 
 vim.lsp.config.nixd = {
   on_attach = utils.on_attach,
   settings = {
       nixd = {
-          nixpkgs = {                                                                                                                           expr = 'let ctx = import ' .. config_dir .. '/nixd/_nixd-expr.nix { self = "dummy"; }; in if ctx.local != null then
-  ctx.local.inputs.nixpkgs else import <nixpkgs> { }',
+          nixpkgs = {
+              expr = 'let ctx = import ' .. config_dir .. '/nixd/_nixd-expr.nix { self = "dummy"; }; in if ctx.local != null then ctx.local.inputs.' .. nixpkgs_input .. ' else import <nixpkgs> { }',
           },
           formatting = {
               command = { "nixfmt" },
